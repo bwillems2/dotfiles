f5551084309e8dfd7269f4e0cfb6832bbf74da1e
Ben Willemsen <ben.willemsen@anz.com>
2022-07-19 11:30:17 +1000

    Put LESS color overrides back in, works with programs that produce man-like output that aren't man

--- zsh/colours.zsh
+++ zsh/colours.zsh
@@ -6,13 +6,13 @@ CLICOLOR="1"
 
 # support colors in less
 # NOTE now using colored-man-pages oh-my-zsh plugin :)
-#export LESS_TERMCAP_mb=$'\E[01;31m'
-#export LESS_TERMCAP_md=$'\E[01;31m'
-#export LESS_TERMCAP_me=$'\E[0m'
-#export LESS_TERMCAP_se=$'\E[0m'
-#export LESS_TERMCAP_so=$'\E[01;44;33m'
-#export LESS_TERMCAP_ue=$'\E[0m'
-#export LESS_TERMCAP_us=$'\E[01;32m'
+export LESS_TERMCAP_mb=$'\E[01;31m'
+export LESS_TERMCAP_md=$'\E[01;31m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;44;33m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;32m'
 
 #export LSCOLORS CLICOLOR LS_COLORS
 export CLICOLOR 
